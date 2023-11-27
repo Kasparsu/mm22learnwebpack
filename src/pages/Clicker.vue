63ed771414935a883b2a6fa9e9febe0b111a04ff
MeriliisT11 <m3riliis.toom3t@gmail.com>
2023-11-27 11:41:13 +0200

    cookieclicker

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -1,56 +1,108 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <p class="text-center">Cookies {{ cookies }}</p>
-      <img src="https://pngimg.com/d/cookie_PNG13656.png" class="img-fluid" @click="cookies++">
-    </div>
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      <button v-for="(upgrade, name) in upgrades"
-        class="btn btn-outline-primary py-3"
-        :disabled="cookies<upgrade.price"
-        @click="buyUpgrade(upgrade)">
-        Buy {{ name }} for 
-        {{ upgrade.price }} clicks ({{upgrade.cps}} clicks per second)
-        {{ upgrade.count }}
-      </button>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-4 mb-4">
+        <div class="card text-center">
+          <div class="card-body">
+            <p class="h4">Cookies: {{ cookies }}</p>
+            <img src="https://pngimg.com/d/cookie_PNG13656.png" class="img-fluid cookie-img" @click="cookies++">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card text-center">
+          <div class="card-body">
+            <button class="btn btn-warning btn-golden" @click="goldenButtonClick">
+              Buy Golden Button for 5 cursors
+            </button>
+            <p class="mt-2">Golden Button Clicks: {{ goldenButtonCount }}</p>
+            <button class="btn btn-success mt-2" @click="buyFreeCookies" :disabled="goldenButtonCount < 5">
+              Buy 10000 Free Clicks for 5 Golden Buttons
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card text-center">
+          <div class="card-body">
+            <button v-for="(upgrade, name) in upgrades" :key="name"
+              class="btn btn-primary btn-upgrade mb-2"
+              :disabled="cookies < upgrade.price"
+              @click="buyUpgrade(upgrade)">
+              Buy {{ name }} for {{ upgrade.price }} clicks
+              <br>({{ upgrade.cps }} clicks per second)
+              <br>Count: {{ upgrade.count }}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+<style>
+.cookie-img {
+  cursor: pointer;
+}
+
+.btn-upgrade {
+  width: 100%;
+}
+</style>
+
 <script>
 export default {
-    created(){
-        setInterval(()=> {
-            for(const upgrade in this.upgrades){
-                this.cookies = (
-                    parseFloat(this.cookies) + this.upgrades[upgrade].cps * this.upgrades[upgrade].count
-                ).toFixed(1);
-            }
-        }, 1000);
+  created() {
+    setInterval(this.updateCookiesAutomatically, 1000);
+  },
+  data() {
+    return {
+      cookies: 0,
+      upgrades: {
+        cursor: { price: 10, cps: 10, count: 0 },
+        grandma: { price: 100, cps: 50, count: 0 },
+        farm: { price: 1000, cps: 100, count: 0 },
+        factory: { price: 10000, cps: 500, count: 0 },
+        temple: { price: 100000, cps: 1000, count: 0 },
+      },
+      goldenButtonCount: 0,
+    };
+  },
+  methods: {
+    buyUpgrade(upgrade) {
+      if (this.canAffordUpgrade(upgrade)) {
+        this.cookies -= upgrade.price;
+        upgrade.price += Math.ceil(upgrade.price * 0.25);
+        upgrade.count++;
+      }
     },
-    data(){
-        return {
-            cookies: 0,
-            upgrades: {
-               cursor: {price: 10, cps: 0.1, count: 0},
-               grandma: {price: 100, cps: 1, count: 0},
-               farm: {price: 1000, cps: 10, count: 0},
-               factory: {price: 10000, cps: 100, count: 0},
-               temple: {price: 100000, cps: 1000, count: 0},
-            }
-        }
+    canAffordUpgrade(upgrade) {
+      return this.cookies >= upgrade.price;
     },
-    methods: {
-        buyUpgrade(upgrade){
-            if(this.cookies>=upgrade.price){
-                this.cookies-=upgrade.price;
-                upgrade.price += Math.ceil(upgrade.price*0.25)         
-                upgrade.count++;
-            }
-        }
-    }
-}
+    updateCookiesAutomatically() {
+      for (const upgrade in this.upgrades) {
+        const cps = this.upgrades[upgrade].cps * this.upgrades[upgrade].count;
+        this.cookies += cps;
+      }
+    },
+    goldenButtonClick() {
+      if (this.upgrades.cursor.count >= 5) {
+        this.upgrades.cursor.count -= 5;
+        this.goldenButtonCount++;
+        // Perform any other actions related to the golden button click
+      } else {
+        alert("Not enough cursors to click the Golden Button!");
+      }
+    },
+    buyFreeCookies() {
+      if (this.goldenButtonCount >= 5) {
+        this.goldenButtonCount -= 5;
+        this.cookies += 10000; // Adjust as needed
+        // Perform any other actions related to buying free cookies
+      } else {
+        alert("You need at least 5 Golden Buttons to buy 1000 Free Cookie Clicks!");
+      }
+    },
+  },
+};
 </script>
